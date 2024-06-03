a4e0b1e56bfc62d563c047afdac361aea7bd0279
Muhammad Hadeed Khan <166932393+Had33dKhan@users.noreply.github.com>
2024-06-03 12:58:40 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lms/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.lms
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webview = findViewById<WebView>(R.id.WebView)
+        webViewSetup(webview)
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetup(webView: WebView){
+        webView.webViewClient = WebViewClient()
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled=true
+            loadUrl("https://mail.dsu.edu.pk")
+
+        }
+
+    }
+}
